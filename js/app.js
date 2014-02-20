6439a1355ed956dc29863ee900dafee19425770f
Leo <leonardobauza@gmail.com>
2014-02-20 08:29:36 -0500

    build routes for products a resource route

--- js/app.js
+++ js/app.js
@@ -4,9 +4,14 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
 	//no need for index route...
-	
+	//normal routes
 	this.route('about'); //the about path renders the about tpl
 	//this.route('about', { path: '/aboutus' }); //specify a different path
+
+	//resource route
+	this.resource('products');
+	//this.resource('products', {path: '/items'});
+
 });
 
 
@@ -17,3 +22,31 @@ App.IndexController = Ember.Controller.extend({
 		return (new Date()).toDateString();
 	}.property()
 });
+
+App.PRODUCTS = [
+	{
+		title: 'Flint',
+		price: 99,
+		description: 'Flint is..',
+		isOnSale: true,
+		image: 'flint.png'
+	},
+	{
+		title: 'Kindling',
+		price: 249,
+		description: 'Easily..',
+		isOnSale: false,
+		image: 'kindling.png'
+	}
+];
+
+App.ProductsRoute = Ember.Route.extend({
+	model: function() {
+		return App.PRODUCTS;
+	}
+});
+
+
+
+
+
