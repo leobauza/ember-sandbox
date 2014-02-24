97f74a0e153f4ef6d2be2619471eb085cec4016b
Leo <leonardobauza@gmail.com>
2014-02-23 21:08:48 -0500

    add contacts and use rest-like set up

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var App = Ember.Application.create({
 	LOG_TRANSITIONS: true //helpful for debugging
 });
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+//App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.ApplicationAdapter = DS.RESTAdapter.extend(); //REST adapter
 
 /*
 
@@ -26,6 +27,10 @@ App.Router.map(function() {
 	this.resource('products', function() {
 		this.resource('product', {path: '/:product_id'});
 	});
+	
+	this.resource('contacts', function() {
+		this.resource('contact', {path: '/:contact_id'});
+	});
 
 });
 
@@ -39,30 +44,54 @@ App.Product = DS.Model.extend({
 	price: DS.attr('number'),
 	description: DS.attr('string'),
 	isOnSale: DS.attr('boolean'),
-	image: DS.attr('string')
+	image: DS.attr('string'),
+	reviews: DS.hasMany('review', {async: true}) //relationship to reviews stablished
 });
 
+App.Review = DS.Model.extend({
+	text: DS.attr('string'),
+	reviewedAt: DS.attr('date'),
+	product: DS.belongsTo('product') //establishes the relationship to product
+});
+
+App.Contact = DS.Model.extend({
+	name: DS.attr('string'),
+	about: DS.attr('string'),
+	avatar: DS.attr('string')
+});
 
 //ember data fixetures for Products
-App.Product.FIXTURES = [ //needs to use the FIXTURES constant within the model
-	{
-		id: 1, //need to give each product a unique ID
-		title: 'Flint',
-		price: 99,
-		description: 'Flint is..',
-		isOnSale: true,
-		image: 'flint.png'
-	},
-	{
-		id: 2,
-		title: 'Kindling',
-		price: 249,
-		description: 'Easily..',
-		isOnSale: false,
-		image: 'kindling.png'
-	}
-];
-
+// App.Product.FIXTURES = [ //needs to use the FIXTURES constant within the model
+// 	{
+// 		id: 1, //need to give each product a unique ID
+// 		title: 'Flint',
+// 		price: 99,
+// 		description: 'Flint is..',
+// 		isOnSale: true,
+// 		image: 'flint.png',
+// 		reviews: [100, 101]
+// 	},
+// 	{
+// 		id: 2,
+// 		title: 'Kindling',
+// 		price: 249,
+// 		description: 'Easily..',
+// 		isOnSale: false,
+// 		image: 'kindling.png'
+// 	}
+// ];
+// App.Review.FIXTURES = [
+// 	{
+// 		id: 100,
+// 		product: 1, //map reviews back to a product!
+// 		text: "Started a fire in no time!"
+// 	},
+// 	{
+// 		id: 101,
+// 		product: 1, //map reviews back to a product!
+// 		text: "Not the brightest flame, but warm!"
+// 	}
+// ];
 
 
 /*
@@ -79,8 +108,13 @@ App.IndexController = Ember.Controller.extend({
 	}.property()
 });
 
-
-
+// App.ContactsIndexController = Ember.Controller.extend({
+// 	contactName: 'Anostagia',
+// 	avatar: 'images/avatar.png',
+// 	open: function() {
+// 		return ((new Date()).getDay() === 0) ? "Closed" : "Open";
+// 	}.property()
+// });
 
 /*
 
@@ -97,14 +131,25 @@ App.ProductsRoute = Ember.Route.extend({
 
 
 //Product Route not needed as this is the ember default behaviour
-// App.ProductRoute = Ember.Route.extend({
-// 	model: function(params) {
-// 		//console.log(params)
-// 		//return App.PRODUCTS.findBy('title', params.title);
-// 		return this.store.find('product', params.product_id);
-// 	}
-// });
+App.ProductRoute = Ember.Route.extend({
+	model: function(params) {
+		//console.log(params)
+		//return App.PRODUCTS.findBy('title', params.title);
+		return this.store.find('product', params.product_id);
+	}
+});
 
+App.ContactsRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.findAll('contact');
+	}
+});
+
+App.ContactRoute = Ember.Route.extend({
+	model: function(params) {
+		return this.store.find('contact', params.contact_id);
+	}
+});
 
 
 
